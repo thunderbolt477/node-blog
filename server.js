e0ca1b0060d6999712493ca4fd6dba830a4b9109
Timothy Balilo <ts.balilo@gmail.com>
2022-11-04 20:11:16 -0700

    updated server

--- server.js
+++ server.js
@@ -1,44 +1,43 @@
 const express = require("express");
-const path = require("path") // require path to use routes
+const path = require("path"); // require path to use routes
+const bodyParser = require("body-parser");
 
+const app = express();
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/my_database");
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-const app = express()
-const ejs = require("ejs")
-const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost/my_database")
-
-
-app.use(express.static("public"))
-app.set("view engine", "ejs")
-
-
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index");
-})
+  res.render("index");
+});
 
-app.get("/about", (req,res) => {
-    res.render("about")
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
-app.get("/contact", (req,res) => {
-    res.render("contact")
-})
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
-app.get("/post", (req,res) => {
-    res.render("post")
-})
+app.get("/post", (req, res) => {
+  res.render("post");
+});
 
 app.get("/posts/new", (req, res) => {
-    res.render("create")
-})
+  res.render("create");
+});
 
-
-
-
+app.post("/posts/store", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
 app.listen(3000, () => {
-    console.log("App listening on port 3000")
-})
+  console.log("App listening on port 3000");
+});
